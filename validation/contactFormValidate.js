36fe60eced976e0fae8d108357f1404019de6f3a
sawan_mak <sawanmakwana305@gmail.com>
2020-09-12 13:14:32 +0530

    admin

--- validation/contactFormValidate.js
+++ validation/contactFormValidate.js
@@ -1,13 +1,13 @@
 
 const Joi = require("joi");
 
-export const validateGenere = genere => {
+export const contactFromValidate = contact => {
     const schema = {
       name: Joi.string()
         .min(3)
         .required()
     };
   
-    return Joi.validate(genere, schema);
+    return Joi.validate(contact, schema);
   };
   
